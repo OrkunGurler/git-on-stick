8403b4983dc9214c3825c99e0b91ebaadb2f87a3
Orkun Gürler <orkungurler@gmail.com>
2020-07-18 21:02:02 +0300

    change module name, import gos to main

---
+++ gos.py
@@ -0,0 +1,13 @@
+
+def s_init():
+    return
+
+def s_clone():
+    print('hello')
+    return
+
+def s_push():
+    return
+
+def s_pull():
+    return
---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import platform
+import getpass
+from gos import *
+
+def genData():
+    cList = {}
+    arguments = sys.argv
+    argsCount = len(sys.argv)
+    if not((argsCount == 1) or (argsCount > 5)):
+        for i in range(1, argsCount):
+            argSplitted = arguments[i].split('=')
+            if not(argSplitted):
+                cList[argSplitted[0]] = argSplitted[1]
+    else:
+        print('Missing or Much Arguments!')
+        sys.exit()
+
+    platformList = {'AIX': 'aix', 'FreeBSD': 'freebsd', 'Linux': 'linux', 'Windows': 'win32', 'Windows/Cygwin': 'cygwin', 'macOS': 'darwin'}
+    platformName = sys.platform
+    flag = 1
+    for value in platformList.values():
+        if platformName == value:
+            cList['--platform'] = platformName
+            flag = 0
+            break
+    if flag:
+        print('Unfortunately Your Platform Is NOT Supported!')
+    return cList
+
+def gosfunc(garg):
+    switcher = {
+        'clone':s_clone(),
+        'init':s_init(),
+        'pull':s_pull(),
+        'push':s_push()
+    }
+    return switcher.get(garg)
+
+def main(data):
+    gosfunc(sys.argv[1])
+    return 1
+    
+
+if __name__ == "__main__":
+    data = genData()
+    main(data)
